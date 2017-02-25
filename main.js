c308c44e7ea467dfdd0f28e560a0dd27bd6c9435
Tijn Kersjes <tijn@divbyzero.nl>
2017-02-25 18:33:52 +0100

    Added CSS transitions

--- main.js
+++ main.js
@@ -10,6 +10,26 @@ function getIntValue(n) {
   return parseInt(n.value, 10);
 }
 
+var fadeTimers = {};
+function updateFade(elem, value, ms) {
+
+  if (elem.value === "") {
+    elem.value = value;
+    return;
+  }
+
+  if (fadeTimers[elem.id])
+    clearTimeout(fadeTimers[elem.id]);
+
+  elem.style.opacity = 0;
+
+  fadeTimers[elem.id] = setTimeout(function () {
+    elem.value = value;
+    elem.style.opacity = 1;
+    fadeTimers[elem.id] = null;
+  }, ms);
+}
+
 function updateNumbers() {
 
   var servingsIn = document.getElementsByName("servingsIn");
@@ -23,8 +43,13 @@ function updateNumbers() {
   var volume = servings * 200;
   var grams = volume * ratio;
 
-  volumeOut.value = volume.toFixed(0);
-  gramsOut.value = grams.toFixed(0);
+  var displayVolume = volume.toFixed(0).toString();
+  if (volumeOut.value !== displayVolume)
+    updateFade(volumeOut, displayVolume, 200);
+
+  var displayGrams = grams.toFixed(0).toString();
+  if (gramsOut.value !== displayGrams)
+    updateFade(gramsOut, displayGrams, 200);
 }
 
 window.onload = function () {
